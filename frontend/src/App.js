220fcff73028fd7b35234aa045ebba91eef4c450
Habib Dwi Prajoto <habibdwi@rekadigi.id>
2023-08-07 15:35:57 +0700

    Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<ProductList/>}/>
+          <Route path="/add" element={<AddProduct/>}/>
+          <Route path="/edit/:id" element={<EditProduct/>}/>
         </Routes>
       </BrowserRouter>
     </div>
